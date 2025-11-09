4fa537a135ff8069a1e36073134768d0cff5140a
KisonHe <kisonmaxine@gmail.com>
2025-11-09 21:20:42 +0800

    Add HexSocketParser

--- README.md
+++ README.md
@@ -8,6 +8,24 @@ All credits go to the original authors of the KCP library: https://github.com/sk
 
 When the `hexfellow` feature is enabled, this library will use the `tokio` crate to handle the UDP socket.
 
+### HexSocket
+
+Made of header and data.
+
+Header in little endian, 4 Bytes
+
+- Byte[0]: 
+  - Higher 4 Bits reserved. Always 0b1000. 
+  - Lower 4 Bits is opcode just like websocket. 
+    - 0x0: Continuation frame(Not used for now, all frames are single frame now)
+    - 0x1: Text frame
+    - 0x2: Binary frame
+    - 0x8: Connection close(Not used for now)
+    - 0x9: Ping
+    - 0xA: Pong
+- Byte[1]: Reserved. Always 0b0000_0000.
+- Byte[2] and Byte[3]: Length of the data in u16.
+
 
 ## TODO
 
--- src/lib.rs
+++ src/lib.rs
@@ -254,3 +254,163 @@ async fn socket_rx(
     );
     Ok(Some(data))
 }
+
+#[cfg(feature = "hexfellow")]
+#[derive(Debug, Eq, PartialEq)]
+enum HexSocketOpcode {
+    // Continuation = 0x0,
+    Text = 0x1,
+    Binary = 0x2,
+    // ConnectionClose = 0x8,
+    Ping = 0x9,
+    Pong = 0xA,
+}
+
+#[cfg(feature = "hexfellow")]
+impl TryFrom<u8> for HexSocketOpcode {
+    type Error = anyhow::Error;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x1 => Ok(Self::Text),
+            0x2 => Ok(Self::Binary),
+            0x9 => Ok(Self::Ping),
+            0xA => Ok(Self::Pong),
+            _ => Err(anyhow::anyhow!("Invalid opcode: {value}")),
+        }
+    }
+}
+
+#[cfg(feature = "hexfellow")]
+struct HexSocketParser {
+    data: Vec<u8>,
+}
+
+#[cfg(feature = "hexfellow")]
+impl HexSocketParser {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+
+    // Panics if data is more than UINT16_MAX bytes.
+    pub fn create_header(data: &[u8], opcode: HexSocketOpcode) -> Vec<u8> {
+        let len = data.len();
+        if len > UINT16_MAX as usize {
+            panic!("Data is more than UINT16_MAX bytes");
+        }
+        let len = len as u16;
+        let mut header = [0u8; 4];
+        header[0] = 0x80 | (opcode as u8);
+        header[1] = 0x00;
+        let len = len.to_le_bytes();
+        header[2..4].copy_from_slice(&len);
+        header.to_vec()
+    }
+
+    // Returns Err is data is bad.
+    // Returns Ok(None) if data is good but not compelete
+    // Returns data if parsed any.
+    pub fn parse(
+        &mut self,
+        incoming: &[u8],
+    ) -> Result<Option<Vec<(HexSocketOpcode, Vec<u8>)>>, anyhow::Error> {
+        let mut ret: Vec<(HexSocketOpcode, Vec<u8>)> = vec![];
+        self.data.extend_from_slice(incoming);
+        loop {
+            let result = self.inner_parse()?;
+            match result {
+                Some((opcode, data)) => {
+                    ret.push((opcode, data));
+                }
+                None => {
+                    if ret.len() > 0 {
+                        return Ok(Some(ret));
+                    } else {
+                        return Ok(None);
+                    }
+                }
+            }
+        }
+    }
+
+    fn inner_parse(&mut self) -> Result<Option<(HexSocketOpcode, Vec<u8>)>, anyhow::Error> {
+        // Check if head if full
+        if self.data.len() < 4 {
+            return Ok(None);
+        }
+        // Validate head
+        let (len, opcode) = if self.data.len() >= 4 {
+            let d = &self.data;
+            if d[0] & 0xF0 != 0x80 {
+                return Err(anyhow::anyhow!("Invalid header: {:?}", d));
+            }
+            let opcode = HexSocketOpcode::try_from(d[0] & 0x0F)?;
+            let len = u16::from_le_bytes([d[2], d[3]]);
+            (len as usize, opcode)
+        } else {
+            return Ok(None);
+        };
+        if self.data.len() >= len + 4 {
+            let (frame, remaining) = self.data.split_at(4 + len);
+            let data = frame[4..].to_vec();
+            self.data = remaining.to_vec();
+            Ok(Some((opcode, data)))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use super::*;
+
+    #[test]
+    fn parse_stream() {
+        let data = "hexfellow".as_bytes();
+        let head = HexSocketParser::create_header(data, HexSocketOpcode::Text);
+        let expected = [129, 0, 9, 0u8];
+        assert_eq!(head, expected);
+
+        // Now create a lot of data, pack them to stream, and decode them out
+        let data = [
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+            "d".to_string(),
+            "1".to_string(),
+            "This is a very long string to test the parser, hahahaha".to_string(),
+        ];
+        let mut stream = vec![];
+        for d in data.clone() {
+            let head = HexSocketParser::create_header(d.as_bytes(), HexSocketOpcode::Text);
+            stream.extend_from_slice(&head);
+            stream.extend_from_slice(d.as_bytes());
+        }
+        // Split stream into small splits
+        let random_lens = [3, 10, 20, 50, 100];
+        for random_len in random_lens {
+            let mut collected = vec![];
+            let mut parser = HexSocketParser::new();
+            let chunks = stream.chunks(random_len).collect::<Vec<_>>();
+            for chunk in chunks {
+                let result = parser.parse(&chunk);
+                if let Some(data) = result.unwrap() {
+                    collected.extend(data);
+                }
+            }
+            let mut texts = vec![];
+            for (opcode, data) in collected {
+                if opcode == HexSocketOpcode::Text {
+                    texts.push(String::from_utf8_lossy(&data).to_string());
+                } else {
+                    panic!("Invalid opcode: {:?}", opcode);
+                }
+            }
+
+            assert_eq!(texts, data);
+            println!("Test passed for length: {:?}", random_len);
+        }
+    }
+}
